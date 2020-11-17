9976ed7f3a73ae80ca1e076d780674650f4fa45c
Maynot2 <paulmanot@gmail.com>
2020-11-17 09:09:17 +0000

    Create hsh header

---
+++ hsh.h
@@ -0,0 +1,16 @@
+#ifndef HSH_H
+#define HSH_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void display_prompt(void);
+char **splitstr(char *);
+char *hsh_readline(void);
+void hsh_exec_cmd(char **);
+int _strlen(char *);
+char **splitstr(char *);
+
+#endif
