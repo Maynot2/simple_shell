bd35b614f5d75fc0cbfdeeaf6a1f8526dc963a0e
Maynot2 <paulmanot@gmail.com>
2020-11-20 09:52:40 +0000

    Create function _puts

--- hsh_print_helpers.c
+++ hsh_print_helpers.c
@@ -1,5 +1,14 @@
 #include "hsh.h"
 
+/**
+ * _putchar - Prints a single character.
+ * @c: A character.
+ *
+ * Return: 1 on success.
+ *         0 if prints nothing.
+ *        -1 on error.
+ *
+ */
 
 int _putchar(char c)
 {
@@ -7,10 +16,24 @@ int _putchar(char c)
 }
 
 /**
-int _puts(char *str)
-{
+ * _puts - Prints a string follow by a newline.
+ * @s: A string.
+ *
+ * Return: nb of printed char on success.
+ *         0 if prints nothing.
+ *        -1 on error.
+ *
+ */
 
-	write(STDOUT_FILENO, &str, _strlen(str));
-	_putchar('\n');
+int _puts(char *s)
+{
+	int count;
+	char *str;
+
+	str = _strcat(s, "\n");
+
+	count = write(STDOUT_FILENO, &str, _strlen(str));
+
+	free(str);
+	return (count);
 }
-*/
